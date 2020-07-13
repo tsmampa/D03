839d428136dbfad84229f3cc0049c5ab41cdb887
Tshephang Mampa <tsmampa@c4r16s1.wethinkcode.co.za>
2020-07-13 12:08:43 +0200

    Div Mod

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -6,8 +6,29 @@
 /*   By: tsmampa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 15:21:02 by tsmampa           #+#    #+#             */
-/*   Updated: 2020/07/10 15:21:07 by tsmampa          ###   ########.fr       */
+/*   Updated: 2020/07/13 09:11:29 by tsmampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+void ft_div_mod(int a, int b, int *div, int *mod);
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	a = 9;
+	b = 2;
+
+	ft_div_mod(
+		printf("a=%d\n,b=%d", c,d)
+		return 0;
+}
