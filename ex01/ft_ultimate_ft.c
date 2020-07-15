68c75c819c01b71a94b5b72d572ac744f618d366
Tshephang Mampa <67946483+tsmampa@users.noreply.github.com>
2020-07-15 13:08:36 +0200

    Update ft_ultimate_ft.c

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -17,21 +17,4 @@ void ft_ultimate_ft(int *********nbr)
  *********nbr = 42;
 }
 
-int main ()
-{
- int a;
- int *ptr1 = &a;
- int **ptr2 = &ptr1;
- int ***ptr3 = &ptr2;
- int ****ptr4 = &ptr3;
- int *****ptr5 = &ptr4;
- int ******ptr6 = &ptr5;
- int *******ptr7 = &ptr6;
- int ********ptr8 = &ptr7;
- int *********ptr9 = &ptr8;
- {
- ft_ultimate_ft(ptr9);
- printf("%d\n",a);
- }
- return (0);
-}
+
